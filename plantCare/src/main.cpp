81a2e7c16641a99eb2bf5c402b3c2369d17fb173
leileiqian <leileiqian@tencent.com>
2022-03-04 20:43:01 +0800

    add plat care project code

---
+++ plantCare/src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 钱磊 on 2022/3/4.
+//
+
+#include "main.h"
---
+++ plantCare/src/main.h
@@ -0,0 +1,14 @@
+//
+// Created by 钱磊 on 2022/3/4.
+//
+
+#ifndef PLANTCARE_MAIN_H
+#define PLANTCARE_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //PLANTCARE_MAIN_H
