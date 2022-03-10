14de1dca619e800a8e62d86ec232df2e0f9e651f
leileiqian <leileiqian@tencent.com>
2022-03-10 20:36:12 +0800

    upate

--- plantCare/src/main.cpp
+++ plantCare/src/main.cpp
@@ -2,4 +2,19 @@
 // Created by 钱磊 on 2022/3/4.
 //
 
-#include "main.h"
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("8266 Begin!!!");
+    pinMode(BUILTIN_LED, OUTPUT);
+}
+
+
+void loop() {
+
+    digitalWrite(BUILTIN_LED, LOW);
+    delay(1000);
+    digitalWrite(BUILTIN_LED, HIGH);
+    delay(2000);
+}
