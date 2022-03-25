6fd49344dd987da4dbf94580bdbdc56a9a5a0f5a
onlinelei <onlinelei@sina.cn>
2022-03-25 12:57:12 +0800

    feat： update plant care project

--- plantCare/src/main.cpp
+++ plantCare/src/main.cpp
@@ -3,11 +3,24 @@
 //
 
 #include <Arduino.h>
+#include <TFT_eSPI.h>
+#include <SPI.h>
+
+
+TFT_eSPI tft = TFT_eSPI();
+int a = 0;
 
 void setup() {
     Serial.begin(115200);
     Serial.println("8266 Begin!!!");
     pinMode(BUILTIN_LED, OUTPUT);
+
+    tft.begin();
+    tft.setRotation(3);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextSize(1);
+    tft.setTextColor(TFT_WHITE);
+    tft.setCursor(0, 0);
 }
 
 
@@ -16,5 +29,14 @@ void loop() {
     digitalWrite(BUILTIN_LED, LOW);
     delay(1000);
     digitalWrite(BUILTIN_LED, HIGH);
+
+    tft.setTextSize(1);
+    tft.setTextColor(RANDOM_REG32);
+//    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(0, 0);
+    tft.println("hello");
+
+
+
     delay(2000);
 }
