5451353b7f5e96b736748c628fee365d493cb9a0
onlinelei <onlinelei@sina.cn>
2022-03-25 15:45:44 +0800

    feat: updagte motor control

--- plantCare/src/main.cpp
+++ plantCare/src/main.cpp
@@ -1,20 +1,24 @@
 //
 // Created by 钱磊 on 2022/3/4.
-//
 
 #include <Arduino.h>
 #include <TFT_eSPI.h>
-#include <SPI.h>
 
-
-TFT_eSPI tft = TFT_eSPI();
 int a = 0;
+// GPIO4 控制电机 mos 管
+int MOTOR_PIN = 4;
+TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
+    // 设置串口速率
     Serial.begin(115200);
     Serial.println("8266 Begin!!!");
-    pinMode(BUILTIN_LED, OUTPUT);
 
+    // 设置引脚状态
+    pinMode(BUILTIN_LED, OUTPUT);
+    pinMode(MOTOR_PIN, OUTPUT);
+
+    // 初始化屏幕
     tft.begin();
     tft.setRotation(3);
     tft.fillScreen(TFT_BLACK);
@@ -26,17 +30,25 @@ void setup() {
 
 void loop() {
 
+    // 控制led
     digitalWrite(BUILTIN_LED, LOW);
-    delay(1000);
+    delay(200);
     digitalWrite(BUILTIN_LED, HIGH);
 
+
+    // 控制电机
+    delay(1000);
+    digitalWrite(MOTOR_PIN, HIGH);
+    // 浇水时间 30 秒
+    delay(1000 * 1);
+    digitalWrite(MOTOR_PIN, LOW);
+
+    // 更新屏幕
+    tft.setCursor(0, 0);
     tft.setTextSize(1);
     tft.setTextColor(RANDOM_REG32);
-//    tft.fillScreen(TFT_BLACK);
-    tft.setCursor(0, 0);
     tft.println("hello");
 
-
-
-    delay(2000);
+    // 休息时间
+    delay(1000 * 2);
 }
